b830d641ec74a69fc3ffdcc8e1cc6272eaa18d68
Jochen Mandl <mandl.js@googlemail.com>
2017-11-16 09:20:59 +0100

    Change PHPDoc from ClientManager

--- src/Supervisor/ClientManager.php
+++ src/Supervisor/ClientManager.php
@@ -9,6 +9,42 @@ use FragSeb\Supervisor\Exception\ClientBadCallException;
 use FragSeb\Supervisor\Response\Response;
 use FragSeb\Supervisor\Response\ResponseInterface;
 
+/**
+ * @method ResponseInterface getAPIVersion()
+ * @method ResponseInterface getSupervisorVersion()
+ * @method ResponseInterface getIdentification()
+ * @method ResponseInterface getState()
+ * @method ResponseInterface getPID()
+ * @method ResponseInterface readLog(int $offset, int $length = 0)
+ * @method ResponseInterface clearLog()
+ * @method ResponseInterface shutdown()
+ * @method ResponseInterface restart()
+ * @method ResponseInterface reloadConfig()
+ * @method ResponseInterface getProcessInfo(string $processName)
+ * @method ResponseInterface getAllProcessInfo()
+ * @method ResponseInterface startAllProcesses(bool $wait = true)
+ * @method ResponseInterface startProcess(string $processName, bool $wait = true)
+ * @method ResponseInterface startProcessGroup(string $groupName, bool $wait = true)
+ * @method ResponseInterface stopAllProcesses(bool $wait = true)
+ * @method ResponseInterface stopProcess(string $processName, bool $wait = true)
+ * @method ResponseInterface restartProcess(string $processName, bool $wait = true)
+ * @method ResponseInterface stopProcessGroup(string $groupName, bool $wait = true)
+ * @method ResponseInterface sendProcessStdin(string $processName, string $chars = 'utf-8')
+ * @method ResponseInterface sendRemoteCommEvent(string $eventType, string $eventData)
+ * @method ResponseInterface addProcessGroup(string $processName)
+ * @method ResponseInterface removeProcessGroup(string $processName)
+ * @method ResponseInterface readProcessStdoutLog(string $processName, int $offset, int $length)
+ * @method ResponseInterface readProcessStderrLog(string $processName, int $offset, int $length)
+ * @method ResponseInterface tailProcessStdoutLog(string $processName, int $offset, int $length)
+ * @method ResponseInterface tailProcessStderrLog(string $processName, int $offset, int $length)
+ * @method ResponseInterface clearProcessLogs(string $processName)
+ * @method ResponseInterface clearAllProcessLogs()
+ * @method ResponseInterface listMethods()
+ * @method ResponseInterface methodHelp(string $methodName)
+ * @method ResponseInterface methodSignature(string $methodSignature)
+ * @method ResponseInterface multicall(array $calls)
+ * @method ResponseInterface getAllConfigInfo()
+ */
 class ClientManager implements ManagerInterface
 {
     /**
